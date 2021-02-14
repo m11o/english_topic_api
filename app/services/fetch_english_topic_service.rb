864f356334fd845884d8b47087851c5c5fa4d892
masahito1997 <velonica.gale2227@gmail.com>
2021-02-14 20:00:04 +0900

    add fetch topic service

---
+++ app/services/fetch_english_topic_service.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'nokogiri'
+require 'open-uri'
+
+class FetchEnglishTopicService
+  FETCH_URL = 'http://iteslj.org/questions/'
+  CATEGORY_SELECTOR = 'table > tr ul > li > a'
+  TOPIC_SELECTOR = 'div.main.wide > ul > li'
+
+  SLEEP_TIME = 30
+
+  def self.call!
+    new.send :call!
+  end
+
+  private
+
+  def call!
+    create_category!
+
+    sleep SLEEP_TIME
+
+    Category.all.find_each do |category|
+      doc = Nokogiri::HTML(URI.open(category.url))
+
+      doc.css(TOPIC_SELECTOR).each do |link|
+        topic = category.topics.find_or_initialize_by name: link.children.first.text
+        topic.save!
+      end
+    end
+  end
+
+  def create_category!
+    doc = Nokogiri::HTML(URI.open(FETCH_URL))
+    doc.css(CATEGORY_SELECTOR).each do |link|
+      category = Category.find_or_initialize_by name: link.text
+      category.url = FETCH_URL + link[:href]
+      category.save!
+    end
+  end
+end
