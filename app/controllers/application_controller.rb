0e854ae51de827e7a2c470c7f5b0f8c61321bae6
masahito1997 <velonica.gale2227@gmail.com>
2021-02-16 20:56:24 +0900

    change find_or_initialize_by

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::API
   before_action :set_user
 
   def set_user
-    @user = User.find_by! mac_address: params[:mac_address]
+    @user = User.find_or_initialize_by mac_address: params[:mac_address]
+    @user.save! if @user.new_record?
   end
 end
