831f01bcdc80b3ebdd1e5e3fb0567ffc84849356
masahito1997 <velonica.gale2227@gmail.com>
2021-02-16 20:48:29 +0900

    refactor topics_controller

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,7 @@
 class TopicsController < ApplicationController
   def show
-    @topic = Topic.random.first
+    already_written_topic = TopicHistory.where(user: @user).pluck(:topic_id)
+    @topic = Topic.where.not(id: already_written_topic).random.first
     render json: { category_name: @topic.category.name, topic: @topic.name }
   end
 end
