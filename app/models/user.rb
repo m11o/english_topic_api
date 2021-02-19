8d94bfd9724cdb9c51b97076380deb3c41373600
masahito1997 <velonica.gale2227@gmail.com>
2021-02-19 17:04:34 +0900

    fix validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
-  validates :mac_address, presence: true
+  validates :code, presence: true
 
   has_many :topic_histories, dependent: :destroy
+
+  before_validation :generate_code
+
+  private
+
+  def generate_code
+    return if code.present?
+
+    self.code = SecureRandom.uuid
+  end
 end
